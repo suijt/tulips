b79d8e395522d056e94054e90d8b6e264f872b3d
suijt <st.zimi1@gmail.com>
2023-08-10 15:30:05 +0545

    database file added

---
+++ tulips.sql
@@ -0,0 +1,121 @@
+create table countries
+(
+    id         bigint unsigned auto_increment
+        primary key,
+    name       varchar(255) not null,
+    created_at timestamp    null,
+    updated_at timestamp    null
+)
+    collate = utf8mb4_unicode_ci;
+
+create table cities
+(
+    id         bigint unsigned auto_increment
+        primary key,
+    country_id bigint unsigned not null,
+    name       varchar(255)    not null,
+    created_at timestamp       null,
+    updated_at timestamp       null,
+    constraint cities_country_id_foreign
+        foreign key (country_id) references countries (id)
+            on update cascade on delete cascade
+)
+    collate = utf8mb4_unicode_ci;
+
+create table failed_jobs
+(
+    id         bigint unsigned auto_increment
+        primary key,
+    uuid       varchar(255)                        not null,
+    connection text                                not null,
+    queue      text                                not null,
+    payload    longtext                            not null,
+    exception  longtext                            not null,
+    failed_at  timestamp default CURRENT_TIMESTAMP not null,
+    constraint failed_jobs_uuid_unique
+        unique (uuid)
+)
+    collate = utf8mb4_unicode_ci;
+
+create table migrations
+(
+    id        int unsigned auto_increment
+        primary key,
+    migration varchar(255) not null,
+    batch     int          not null
+)
+    collate = utf8mb4_unicode_ci;
+
+create table password_reset_tokens
+(
+    email      varchar(255) not null
+        primary key,
+    token      varchar(255) not null,
+    created_at timestamp    null
+)
+    collate = utf8mb4_unicode_ci;
+
+create table password_resets
+(
+    email      varchar(255) not null,
+    token      varchar(255) not null,
+    created_at timestamp    null
+)
+    collate = utf8mb4_unicode_ci;
+
+create index password_resets_email_index
+    on password_resets (email);
+
+create table personal_access_tokens
+(
+    id             bigint unsigned auto_increment
+        primary key,
+    tokenable_type varchar(255)    not null,
+    tokenable_id   bigint unsigned not null,
+    name           varchar(255)    not null,
+    token          varchar(64)     not null,
+    abilities      text            null,
+    last_used_at   timestamp       null,
+    expires_at     timestamp       null,
+    created_at     timestamp       null,
+    updated_at     timestamp       null,
+    constraint personal_access_tokens_token_unique
+        unique (token)
+)
+    collate = utf8mb4_unicode_ci;
+
+create index personal_access_tokens_tokenable_type_tokenable_id_index
+    on personal_access_tokens (tokenable_type, tokenable_id);
+
+create table populations
+(
+    id         bigint unsigned auto_increment
+        primary key,
+    city_id    bigint unsigned            not null,
+    type       enum ('old, young, child') not null,
+    number     bigint                     not null,
+    created_at timestamp                  null,
+    updated_at timestamp                  null,
+    constraint populations_city_id_foreign
+        foreign key (city_id) references cities (id)
+            on update cascade on delete cascade
+)
+    collate = utf8mb4_unicode_ci;
+
+create table users
+(
+    id                bigint unsigned auto_increment
+        primary key,
+    name              varchar(255) not null,
+    email             varchar(255) not null,
+    email_verified_at timestamp    null,
+    password          varchar(255) not null,
+    remember_token    varchar(100) null,
+    created_at        timestamp    null,
+    updated_at        timestamp    null,
+    constraint users_email_unique
+        unique (email)
+)
+    collate = utf8mb4_unicode_ci;
+
+
